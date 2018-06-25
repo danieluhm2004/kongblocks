bc3230cb36432a95b84f2986c687b8c7ad5481c6
Mcraft <admin@mcraft.kr>
2018-06-26 05:11:24 +0900

    기본적인 설정을 하였습니다.

---
+++ src/main/java/kr/mcraft/kongblocks/KongBlocks.java
@@ -0,0 +1,17 @@
+package main.java.kr.mcraft.kongblocks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class KongBlocks extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		super.onEnable();
+	}
+	
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		super.onDisable();
+	}
+}
